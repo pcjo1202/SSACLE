6c3f4808c57e21294e52140b3bef37cd38cb63bd
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2025-01-28 21:51:19 +0900

    feat: 발표 페이지 Header 컴포넌트 추가

---
+++ frontend/src/components/PresentationHeader/PresentationHeader.jsx
@@ -0,0 +1,26 @@
+import Progress from '@/components/Progress/Progress'
+
+const PresentationHeader = () => {
+  const steps = [
+    { step: '준비' },
+    { step: '발표' },
+    { step: '질문' },
+    { step: '평가' },
+    { step: '완료' },
+  ]
+  return (
+    <header className="">
+      <div className="flex items-center justify-between h-20 px-24">
+        {/* logo */}
+        <h1 className="text-2xl font-bold text-sky-400">SSACLE</h1>
+        {/* progress UI */}
+        <Progress //
+          steps={steps}
+          activeStep={1}
+          className="my-4"
+        />
+      </div>
+    </header>
+  )
+}
+export default PresentationHeader
