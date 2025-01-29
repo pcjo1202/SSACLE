536006be8b6dca86ba5d9dc1c6ec7ce58fe8cae5
박창조 <ckdwh1202@gmail.com>
2025-01-30 02:01:20 +0900

    feat: presentationLayout 높이 조정

--- frontend/src/components/layout/PresentationLayout.jsx
+++ frontend/src/components/layout/PresentationLayout.jsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router-dom'
 
 const PresentationLayout = () => {
   return (
-    <div className="flex flex-col w-full h-screen text-white bg-ssacle-black">
+    <div className="flex flex-col w-full h-screen text-white bg-ssacle-black min-w-[1280px]">
       <PresentationHeader />
-      <main className="flex-1 px-24">
+      <main className="flex-1 px-24 bg-ssacle-black h-[calc(100vh-h-20-h-16)]">
         <Outlet />
       </main>
       <PresentationControlBar />
