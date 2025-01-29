423513053ee0b0fab2b02426fa8a378616082a99
hs <hseegr@naver.com>
2025-01-29 21:48:17 +0900

    feat: mainpage UI 구현

---
+++ frontend/src/components/commons/SsadCard.jsx
@@ -0,0 +1,45 @@
+const stackLogos = {
+  React: '/src/mocks/react-logo.png',
+  Python: '/src/mocks/python-logo.png',
+}
+
+const SsadCard = ({ sprintData }) => {
+  const {
+    title,
+    category,
+    status,
+    requiredSkills,
+    currentMembers,
+    maxMembers,
+    startDate,
+    endDate,
+  } = sprintData
+
+  const logoPath =
+    stackLogos[requiredSkills[0]] || '/src/mocks/default-logo.png'
+
+  return (
+    <div className="bg-cyan-400 h-64 w-full rounded-xl p-5  relative">
+      <div className="text-white">
+        <p className="text-4xl font-bold mb-3">{requiredSkills[0]}</p>
+        <p className="text-xl font-medium mb-3">{title}</p>
+        <p className="text-base font-medium">시작일 {startDate}</p>
+        <p className="text-base font-medium">종료일 {endDate}</p>
+        <p className="text-base font-medium mb-3">
+          {status}{' '}
+          <span>
+            {currentMembers} / {maxMembers}
+          </span>
+        </p>
+        <p className="text-cyan-400 text-base font-semibold bg-white w-20 h-8 rounded-md flex items-center justify-center">
+          신청하기
+        </p>
+      </div>
+      <div className="absolute bottom-5 right-5">
+        <img src={logoPath} alt={`${category} logo`} className="w-20 h-20" />
+      </div>
+    </div>
+  )
+}
+
+export default SsadCard
--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,49 @@
+import mockData from '@/mocks/mainPageData.json'
+import MySsaprintList from '@/components/MainPage/Profile/MySsaprintList'
+import Profile from '@/components/MainPage/Profile/Profile'
+import CurruntSsaprint from '@/components/MainPage/Ssaprint/CurrentSsaprint'
+import CurruntSsadcup from '@/components/MainPage/Ssadcup/CurrentSsadcup'
+import SsabapVote from '@/components/MainPage/SsabapVote/SsabapVote'
+import News from '@/components/MainPage/News/News'
+
 const MainPage = () => {
-  return <div></div>
+  const { user, currentSprints, recommendedSprints, aiNews } = mockData
+
+  return (
+    <main className="mx-[200px]">
+      {/* 프로필, 나의 싸프린트 + 싸드컵 현황 보드 */}
+      <div className="flex flex-row gap-x-5 mb-20">
+        <section className="basis-1/3">
+          <Profile userData={user} />
+        </section>
+        <section className="basis-2/3">
+          <MySsaprintList currentSprintsData={currentSprints} />
+        </section>
+      </div>
+      {/* 모집중인 싸프린트 */}
+      <section className="mb-20">
+        <CurruntSsaprint
+          userData={user}
+          recommendedSprints={recommendedSprints}
+        />
+      </section>
+      {/* 모집중인 싸드컵 */}
+      <section className="mb-20">
+        <CurruntSsadcup
+          userData={user}
+          recommendedSprints={recommendedSprints}
+        />
+      </section>
+      {/* 싸밥 투표, AI 기사 */}
+      <div className="flex flex-row gap-x-5">
+        <section className="basis-1/2">
+          <SsabapVote />
+        </section>
+        <section className="basis-1/2">
+          <News news={aiNews} />
+        </section>
+      </div>
+    </main>
+  )
 }
 export default MainPage
