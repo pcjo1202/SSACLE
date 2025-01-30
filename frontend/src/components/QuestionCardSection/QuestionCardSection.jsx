bfbdd11bd8c7d430d316e68e173b360fba87109c
박창조 <ckdwh1202@gmail.com>
2025-01-31 00:20:23 +0900

    feat: 질문 카드 페이지 컴포넌트 추가
    - 질문 카드 컴포넌트 추가
    - 질문 카드 섹션 컴포넌트 추가
    - 질문 카드 페이지 컴포넌트 추가

---
+++ frontend/src/components/QuestionCardSection/QuestionCardSection.jsx
@@ -0,0 +1,14 @@
+import QuestionCard from '@/components/QuestionCard/QuestionCard'
+
+const QuestionCardSection = () => {
+  return (
+    <div className="basis-1/4 w-full h-full border-[1px] border-gray-500 rounded-md">
+      <ul className="flex justify-center w-full h-full gap-4 p-2">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <QuestionCard key={index} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+export default QuestionCardSection
--- frontend/src/pages/PresentationPage/QuestionCardPage.jsx
+++ frontend/src/pages/PresentationPage/QuestionCardPage.jsx
@@ -1,4 +1,12 @@
-const QuestionCardPage = ({ stream = [] }) => {
-  return <div></div>
+import QuestionCardSection from '@/components/QuestionCardSection/QuestionCardSection'
+import PresentingPage from '@/pages/PresentationPage/PresentingPage'
+
+const QuestionCardPage = () => {
+  return (
+    <div className="flex flex-col w-full h-full gap-2">
+      <QuestionCardSection />
+      <PresentingPage />
+    </div>
+  )
 }
 export default QuestionCardPage
