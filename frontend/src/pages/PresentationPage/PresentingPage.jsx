d73efa50f226f296c2d9742e8c35294ee0d70d3d
박창조 <ckdwh1202@gmail.com>
2025-01-31 00:17:34 +0900

    feat: 발표 중 페이지 컴포넌트 추가

--- frontend/src/pages/PresentationPage/PresentingPage.jsx
+++ frontend/src/pages/PresentationPage/PresentingPage.jsx
@@ -1,4 +1,14 @@
-const PresentingPage = ({ stream = [] }) => {
-  return <div></div>
+import ParticipantSection from '@/components/ParticipantSection/ParticipantSection'
+import SharedSection from '@/components/SharedSection/SharedSection'
+
+const PresentingPage = () => {
+  return (
+    <div className="flex w-full h-full">
+      {/* 공유 영역 */}
+      <SharedSection />
+      {/* 참여자 영역 */}
+      <ParticipantSection />
+    </div>
+  )
 }
 export default PresentingPage
