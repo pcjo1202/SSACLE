da9ddea1da4903c405672aeffd892fdd67e0a202
박창조 <ckdwh1202@gmail.com>
2025-01-31 00:23:10 +0900

    feat: 스트림 비디오 카드 컴포넌트 CSS 수정

--- frontend/src/components/StreamVideoCard/StreamVideoCard.jsx
+++ frontend/src/components/StreamVideoCard/StreamVideoCard.jsx
@@ -1,20 +1,24 @@
+import { cn } from '@/lib/utils'
+
 const StreamVideoCard = ({ data }) => {
   const { stream, name } = data
 
   return (
-    <div className="flex items-center justify-center w-full">
-      <div className="relative aspect-[3/2] w-full rounded-md shadow-md border-[1px] border-gray-500">
-        {/* 참여자 영상 */}
-        <video
-          autoPlay
-          playsInline
-          className="relative object-cover w-full h-full "
-          ref={stream}
-        ></video>
-        {/* 참여자 이름 */}
-        <div className="absolute bottom-0 flex items-center justify-center w-1/4 bg-white rounded-sm opacity-30">
-          <span className="text-sm text-ssacle-black">{name}</span>
-        </div>
+    <div
+      className={cn(
+        'relative size-full mx-auto rounded-md shadow-md border-[1px] border-gray-500'
+      )}
+    >
+      {/* 참여자 영상 */}
+      <video
+        autoPlay
+        playsInline
+        className="object-cover w-full h-full rounded-md"
+        ref={stream}
+      ></video>
+      {/* 참여자 이름 */}
+      <div className="absolute bottom-0 flex items-center justify-center w-1/4 bg-white rounded-sm opacity-30">
+        <span className="text-sm text-ssacle-black">{name}</span>
       </div>
     </div>
   )
