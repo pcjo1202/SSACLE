d18794a8c0aa334f426e5e465d31e48ae9a2ccb6
박창조 <ckdwh1202@gmail.com>
2025-01-31 00:16:50 +0900

    feat: 공유 영역, 참여자 영역 컴포넌트 추가

---
+++ frontend/src/components/ParticipantSection/ParticipantSection.jsx
@@ -0,0 +1,16 @@
+import StreamVideoCard from '@/components/StreamVideoCard/StreamVideoCard'
+import { usePresentation } from '@/store/usePresentation'
+
+const ParticipantSection = () => {
+  const { stream } = usePresentation()
+  return (
+    <div className="w-full h-full basis-1/5 border-[1px] border-gray-500 rounded-md">
+      <section className="flex flex-col items-center justify-around h-full gap-2 p-2">
+        {stream.map((item, index) => (
+          <StreamVideoCard key={index} data={item} />
+        ))}
+      </section>
+    </div>
+  )
+}
+export default ParticipantSection
