34c85a80ecb78b5c7511310440d44e4cb40b5d3e
aaange <ayung00@naver.com>
2025-01-31 17:48:38 +0900

    style:signup 코드 정리 - 컬러 코드 tailwind로 수정 - px단위 rem 단위로 수정

--- frontend/src/pages/Account/SignupPage/SignupStep1.jsx
+++ frontend/src/pages/Account/SignupPage/SignupStep1.jsx
@@ -6,10 +6,10 @@ const SignupStep1 = () => {
   const [showCodeInput, setShowCodeInput] = useState(false)
 
   return (
-    <div className="w-full h-auto flex justify-center items-center px-[200px] mt-[200px]">
+    <div className="w-full h-auto flex justify-center items-center mt-24">
       <div className="grid grid-cols-12 gap-4 w-full">
         <div className="col-span-4 col-start-5">
-          <h1 className="text-[#5195F7] text-3xl font-bold text-center mb-10">
+          <h1 className="text-ssacle-blue text-3xl font-bold text-center mb-10">
             회원가입
           </h1>
 
@@ -17,7 +17,7 @@ const SignupStep1 = () => {
           <input
             type="email"
             placeholder="Mattermost를 가입한 이메일을 입력해주세요"
-            className="w-full h-[50px] bg-[#f4f4f4] rounded-full px-6 text-[#5195F7] text-medium text-base focus:outline-[#5195F7] mb-4"
+            className="w-full h-12 bg-ssacle-gray-sm rounded-full px-6 text-ssacle-blue text-medium text-base focus:outline-ssacle-blue mb-4"
           />
 
           {/* 인증 코드 입력 필드 (버튼 클릭시 출력) */}
@@ -25,16 +25,16 @@ const SignupStep1 = () => {
             <input
               type="text"
               placeholder="인증 코드를 입력하세요"
-              className="w-full h-[50px] bg-[#f4f4f4] rounded-full px-6 text-[#5195F7] text-base text-medium focus:outline-[#5195F7] mb-4"
+              className="w-full h-12 bg-ssacle-gray-sm rounded-full px-6 text-ssacle-blue text-base text-medium focus:outline-ssacle-blue mb-4"
             />
           )}
 
           {/* 인증 코드 요청 버튼 */}
           <button
-            className={`w-full h-[50px] rounded-full text-center text-xl font-bold mb-4 transition-colors duration-300 ${
+            className={`w-full h-12 rounded-full text-center text-xl font-bold mb-4 transition-colors duration-300 ${
               showCodeInput
-                ? 'bg-[#E5F0FF] text-[#242424]'
-                : 'bg-[#5195F7] text-white'
+                ? 'bg-ssacle-sky text-ssacle-black'
+                : 'bg-ssacle-blue text-white'
             }`}
             onClick={() => setShowCodeInput(true)}
           >
@@ -43,7 +43,7 @@ const SignupStep1 = () => {
 
           {showCodeInput && (
             <button
-              className="w-full h-[50px] bg-[#5195F7] rounded-full px-6 text-white text-center text-xl font-bold mb-4"
+              className="w-full h-12 bg-ssacle-blue rounded-full px-6 text-white text-center text-xl font-bold mb-4"
               onClick={() => navigate('step2')} // 다음 단계로 이동
             >
               SSAFY인 인증하기
