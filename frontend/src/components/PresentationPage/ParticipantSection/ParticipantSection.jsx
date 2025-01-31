e0c9d9e8b657e48c1de9f9a62d36ad22ef2968c6
박창조 <ckdwhd1202@gmail.com>
2025-01-31 14:15:19 +0900

    feat: 참여자 영역 페이지 캐러셀 구현

--- frontend/src/components/PresentationPage/ParticipantSection/ParticipantSection.jsx
+++ frontend/src/components/PresentationPage/ParticipantSection/ParticipantSection.jsx
@@ -1,15 +1,54 @@
 import StreamVideoCard from '@/components/PresentationPage/StreamVideoCard/StreamVideoCard'
 import { usePresentation } from '@/store/usePresentation'
+import { useMemo, useState } from 'react'
+
+const PAGE_SIZE = 4
 
 const ParticipantSection = () => {
+  const [currentPage, setCurrentPage] = useState(0)
   const { stream } = usePresentation()
+
+  const handlePrevPage = () => {
+    setCurrentPage((prev) => (prev > 0 ? prev - 1 : 0))
+  }
+  const handleNextPage = () => {
+    setCurrentPage((prev) =>
+      prev < stream.length / PAGE_SIZE ? prev + 1 : prev
+    )
+  }
+
+  // 4개씩 보여주기
+  const visitableStream = useMemo(() => {
+    return stream.slice(currentPage * PAGE_SIZE, (currentPage + 1) * PAGE_SIZE)
+  }, [currentPage, stream])
+
   return (
-    <div className="w-full h-full basis-1/5 border-[1px] border-gray-500 rounded-md">
-      <section className="flex flex-col items-center justify-around h-full gap-2 p-2">
-        {stream.map((item, index) => (
-          <StreamVideoCard key={index} data={item} />
-        ))}
-      </section>
+    <div className="w-full h-full basis-1/5 border-[1px] border-gray-500 rounded-md overflow-y-auto flex flex-col">
+      <div className="flex flex-col justify-between h-full">
+        <section className="flex flex-col items-center justify-start flex-1 gap-2 p-2 overflow-hidden">
+          {visitableStream.map((item, index) => (
+            <StreamVideoCard key={index} data={item} />
+          ))}
+        </section>
+        {stream.length > PAGE_SIZE && (
+          <div className="flex items-center justify-center gap-2">
+            <button
+              disabled={currentPage === 0}
+              onClick={handlePrevPage}
+              className="disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              이전
+            </button>
+            <button
+              disabled={currentPage === Math.floor(stream.length / PAGE_SIZE)}
+              onClick={handleNextPage}
+              className="disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              다음
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
