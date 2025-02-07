9acf1af20532cd0b252c124a638d94f9fb5258ac
박창조 <ckdwhd1202@gmail.com>
2025-02-07 10:50:55 +0900

    docs: Fronend 개발 브랜치에서 필요없는 파일 삭제제

--- 개인평가/전성호/7.spring_security/3.OAuthSession/3.OAuthSession/src/main/java/com/OAuthSession3/OAuthSession/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.OAuthSession3.OAuthSession.repository;
-
-import com.OAuthSession3.OAuthSession.domain.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUsername(String username);
-}
--- 개인평가/전성호/7.spring_security/4.OAuth-JWT/src/main/java/com/jun4/OAuth/JWT/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jun4.OAuth.JWT.repository;
-
-
-import com.jun4.OAuth.JWT.domain.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUsername(String username);
-}
