2edfd2b09e92d8544b2ec39ab375beef00680f87
김희수 <hseegr@naver.com>
2025-02-10 16:32:36 +0900

    feat: 로그아웃 기능 구현 중

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,21 @@
-import { Search, Lock, User } from 'lucide-react'
+import { fetchLogout } from '@/services/userService'
+import { Search, Lock, User, LogOut, Cookie } from 'lucide-react'
 import { useNavigate, Link } from 'react-router-dom'
 
 const Header = () => {
   const navigate = useNavigate()
+  const accessToken = localStorage.getItem('accessToken') // 로그인 여부 확인
+
+  // 리프레시 토큰 삭제 함수
+  // 기존 코드
+  // const deleteCookie = (name) => {
+  //   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+  // }
+  // const handleLogout = () => {
+  //   localStorage.removeItem('accessToken') // 액세스 토큰 삭제
+  //   deleteCookie('refreshToken') // 리프레시 토큰 삭제
+  //   window.location.reload() // 페이지 새로고침
+  // }
 
   return (
     <header className="min-w-max z-10 fixed top-0 left-0 w-full h-12 px-48 py-4 bg-white shadow-sm flex justify-between items-center">
@@ -35,12 +48,23 @@ const Header = () => {
         <button className="w-5 h-5 flex justify-center items-center">
           <Search size={15} className="text-black" />
         </button>
-        <button
-          className="w-5 h-5 flex justify-center items-center"
-          onClick={() => navigate('account/login')}
-        >
-          <Lock size={15} className="text-black" />
-        </button>
+
+        {accessToken ? (
+          <button
+            className="w-5 h-5 flex justify-center items-center"
+            onClick={fetchLogout}
+          >
+            <LogOut size={15} className="text-black" />
+          </button>
+        ) : (
+          <button
+            className="w-5 h-5 flex justify-center items-center"
+            onClick={() => navigate('account/login')}
+          >
+            <Lock size={15} className="text-black" />
+          </button>
+        )}
+
         <button
           className="w-5 h-5 flex justify-center items-center"
           onClick={() => navigate('user/profile')}
@@ -51,4 +75,5 @@ const Header = () => {
     </header>
   )
 }
+
 export default Header
