b3d71303bc1d5f55fd123930e92fc10a969aac75
aaaange <ayung00@naver.com>
2025-02-11 21:45:01 +0900

    feat: signup interest UI 수정

--- frontend/src/pages/Account/SignupPage/InterestPage.jsx
+++ frontend/src/pages/Account/SignupPage/InterestPage.jsx
@@ -70,69 +70,75 @@ const InterestPage = () => {
   ]
 
   return (
-    <div className="w-full h-auto bg-white flex flex-col items-center py-10 mt-24">
-      <p className="text-ssacle-gray text-xl font-noto-sans-kr font-light mb-2">
-        거의 다 됐어요!
-      </p>
-      <h2 className="text-ssacle-black text-2xl font-noto-sans-kr font-light mb-2">
-        <span className="font-bold">{userNickname}</span>님의 관심 주제를
-        알려주세요!
-      </h2>
-      <div className="text-center text-ssacle-black text-2xl font-noto-sans-kr font-light mb-2">
-        <p>해당 키워드를 바탕으로,</p>
-        <p>훨씬 수월하게 콘텐츠를 고를 수 있어요!</p>
+    <div className="🔥 w-auto h-auto overflow-auto flex justify-center items-center bg-white">
+      <div className="🔥 w-[40rem] max-w-2xl h-auto bg-white flex flex-col justify-center items-center py-10 mt-24 shrink-0">
+        <p className="text-ssacle-gray text-xl font-noto-sans-kr font-light mb-2">
+          거의 다 됐어요!
+        </p>
+        <h2 className="text-ssacle-black text-2xl font-noto-sans-kr font-light mb-2">
+          <span className="font-bold">{userNickname}</span>님의 관심 주제를
+          알려주세요!
+        </h2>
+        <div className="text-center text-ssacle-black text-2xl font-noto-sans-kr font-light mb-2">
+          <p>해당 키워드를 바탕으로,</p>
+          <p>훨씬 수월하게 콘텐츠를 고를 수 있어요!</p>
+        </div>
+        <p className="text-ssacle-gray text-xl font-noto-sans-kr font-light mb-20">
+          마이페이지에서 언제든 수정할 수 있어요!
+        </p>
+
+        <div className="max-w-2xl">
+          {/* 주요 카테고리 */}
+          <div className="flex flex-wrap justify-center gap-4 w-full mb-10">
+            {mainCategories.map((interest) => (
+              <button
+                key={interest}
+                onClick={() => toggleSelection(interest)}
+                className={`px-6 py-2 rounded-full text-xl font-bold font-montserrat transition-colors duration-200 ${
+                  selected.has(interest)
+                    ? 'bg-ssacle-blue text-white'
+                    : 'bg-ssacle-sky text-ssacle-blue'
+                }`}
+              >
+                {interest}
+              </button>
+            ))}
+          </div>
+
+          {/* 구분선 */}
+          <div className="flex flex-col items-center w-full">
+            <div className="w-6 border-b-4 border-ssacle-gray-sm mb-10"></div>
+          </div>
+
+          {/* 서브 카테고리 */}
+          <div className="flex flex-wrap justify-center gap-4 w-full mb-10">
+            {subCategories.map((interest) => (
+              <button
+                key={interest}
+                onClick={() => toggleSelection(interest)}
+                className={`px-6 py-2 rounded-full text-xl font-bold font-montserrat transition-colors duration-200 ${
+                  selected.has(interest)
+                    ? 'bg-ssacle-blue text-white'
+                    : 'bg-ssacle-sky text-ssacle-blue'
+                }`}
+              >
+                {interest}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        {/* 저장 버튼 */}
+        <button
+          className={`mt-10 mb-24 px-10 py-3 text-white text-xl font-bold font-noto-sans-kr rounded-full transition-all ${
+            userId ? 'bg-ssacle-black' : 'bg-ssacle-gray cursor-not-allowed'
+          }`}
+          onClick={() => userId && saveInterestsMutation.mutate()}
+          disabled={!userId}
+        >
+          {saveInterestsMutation.isLoading ? '저장 중...' : '저장하기'}
+        </button>
       </div>
-      <p className="text-ssacle-gray text-xl font-noto-sans-kr font-light mb-20">
-        마이페이지에서 언제든 수정할 수 있어요!
-      </p>
-
-      {/* 주요 카테고리 */}
-      <div className="flex flex-wrap justify-center gap-4 w-full mb-10">
-        {mainCategories.map((interest) => (
-          <button
-            key={interest}
-            onClick={() => toggleSelection(interest)}
-            className={`px-6 py-2 rounded-full text-xl font-bold font-montserrat transition-colors duration-200 ${
-              selected.has(interest)
-                ? 'bg-ssacle-blue text-white'
-                : 'bg-ssacle-sky text-ssacle-blue'
-            }`}
-          >
-            {interest}
-          </button>
-        ))}
-      </div>
-
-      {/* 구분선 */}
-      <div className="w-6 border-b-4 border-ssacle-gray-sm mb-10"></div>
-
-      {/* 서브 카테고리 */}
-      <div className="flex flex-wrap justify-center gap-4 w-full mb-10">
-        {subCategories.map((interest) => (
-          <button
-            key={interest}
-            onClick={() => toggleSelection(interest)}
-            className={`px-6 py-2 rounded-full text-xl font-bold font-montserrat transition-colors duration-200 ${
-              selected.has(interest)
-                ? 'bg-ssacle-blue text-white'
-                : 'bg-ssacle-sky text-ssacle-blue'
-            }`}
-          >
-            {interest}
-          </button>
-        ))}
-      </div>
-
-      {/* 저장 버튼 */}
-      <button
-        className={`mt-10 mb-24 px-10 py-3 text-white text-xl font-bold font-noto-sans-kr rounded-full transition-all ${
-          userId ? 'bg-ssacle-black' : 'bg-ssacle-gray cursor-not-allowed'
-        }`}
-        onClick={() => userId && saveInterestsMutation.mutate()}
-        disabled={!userId}
-      >
-        {saveInterestsMutation.isLoading ? '저장 중...' : '저장하기'}
-      </button>
     </div>
   )
 }
