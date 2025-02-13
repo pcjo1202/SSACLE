0dc917e691149db960194a674e13265f313e1453
박창조 <ckdwhd1202@gmail.com>
2025-02-13 16:56:06 +0900

    feat: 컨트롤바 드롭다운 추가

--- frontend/src/components/PresentationPage/PresentationControlItem/PresentationControlItem.jsx
+++ frontend/src/components/PresentationPage/PresentationControlItem/PresentationControlItem.jsx
@@ -1,17 +1,28 @@
+import ControlBarDropdown from '@/components/ControlBarDropdown/ControlBarDropdown'
+
 const PresentationControlItem = ({ control }) => {
-  const { icon: Icon, title, style, activeFunction } = control
+  const {
+    icon: Icon,
+    title,
+    style,
+    activeFunction,
+    isDropdown,
+    dropDownItems,
+  } = control
 
   return (
     <li className="list-none">
-      <button
-        className={`flex flex-col  transition-all duration-200 justify-center items-center gap-2 px-4 rounded-md ${style} hover:bg-black/40 py-1`}
-        onClick={activeFunction}
-      >
-        <span className="size-5">
-          <Icon className="w-full" />
-        </span>
-        <span className="text-xs text-ssacle-gray-sm ">{title}</span>
-      </button>
+      <ControlBarDropdown isDropdown={isDropdown} dropDownItems={dropDownItems}>
+        <button
+          className={`flex flex-col  transition-all duration-200 justify-center items-center gap-2 px-4 rounded-md ${style} hover:bg-black/40 py-1`}
+          onClick={activeFunction}
+        >
+          <span className="size-5">
+            <Icon className="w-full" />
+          </span>
+          <span className="text-xs text-ssacle-gray-sm ">{title}</span>
+        </button>
+      </ControlBarDropdown>
     </li>
   )
 }
