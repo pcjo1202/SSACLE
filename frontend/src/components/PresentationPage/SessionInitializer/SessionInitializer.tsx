e593fd13a2a72c33527e9007a4388a2a58a339b7
박창조 <ckdwhd1202@gmail.com>
2025-02-18 17:18:14 +0900

    fix: 테스트를 위한 코드 수정

--- frontend/src/components/PresentationPage/SessionInitializer/SessionInitializer.tsx
+++ frontend/src/components/PresentationPage/SessionInitializer/SessionInitializer.tsx
@@ -4,7 +4,11 @@ import {
   fetchSessionId,
   fetchToken,
 } from '@/services/openviduService'
-import { fetchQuestionCards } from '@/services/presentationService'
+import {
+  fetchPresentationParticipants,
+  fetchQuestionCards,
+} from '@/services/presentationService'
+import { usePresentationStore } from '@/store/usePresentationStore'
 import { useQuery } from '@tanstack/react-query'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
@@ -12,19 +16,42 @@ import { useParams } from 'react-router-dom'
 const SessionInitializer = () => {
   const { roomId } = useParams()
 
+  // 세션 초기화 시 타겟 연결 수 설정
+  const setTargetConnectionCount = usePresentationStore(
+    (state) => state.setTargetConnectionCount
+  )
+
   // 세션 ID 요청
   const { data: token, isSuccess } = useQuery({
     queryKey: ['openvidu-token'],
     queryFn: async () => {
-      const sessionId = await fetchSessionId(roomId ?? 'test-session-id')
-      const token = await fetchToken(sessionId)
-      return token
-      // const serverToken = await fetchServerToken(roomId ?? 'test-session-id')
-      // return serverToken // 실제 데이터만 반환
+      // const sessionId = await fetchSessionId(roomId ?? 'test-session-id')
+      // const token = await fetchToken(sessionId)
+      // return token
+      const serverToken = await fetchServerToken(roomId ?? 'test-session-id')
+      return serverToken // 실제 데이터만 반환
     },
     staleTime: 0, // 토큰은 한번 받으면 변경되지 않으므로
     gcTime: 0,
   })
+
+  // 타겟 연결 수 설정
+  const {
+    data: presentationParticipants,
+    isSuccess: isPresentationParticipantsSuccess,
+  } = useQuery({
+    queryKey: ['presentation-participants'],
+    queryFn: () => fetchPresentationParticipants(roomId ?? 'test-session-id'),
+    staleTime: 0, // 타겟 연결 수는 한번 받으면 변경되지 않으므로
+    enabled: !!roomId,
+  })
+
+  useEffect(() => {
+    if (isPresentationParticipantsSuccess) {
+      console.log('참가자 목록', presentationParticipants)
+      setTargetConnectionCount(presentationParticipants.length)
+    }
+  }, [])
 
   // 질문 카드 목록 요청
   const { data: questionCardList, isSuccess: isQuestionCardListSuccess } =
