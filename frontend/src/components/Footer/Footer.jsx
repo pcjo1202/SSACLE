25e64d42668948adfffe2393a03939ee397b0dd5
박창조 <ckdwhd1202@gmail.com>
2025-02-18 16:52:03 +0900

    fix: 레이아웃 수정

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,18 +2,22 @@ import { Coffee } from 'lucide-react'
 
 const Footer = () => {
   return (
-    <footer className="w-full h-12 px-6 py-10 bg-ssacle-gray-sm flex flex-col justify-center items-center">
-      <div className="flex items-center space-x-1">
-        <span className="text-black text-base font-medium font-montserrat">
-          SSACLE
-        </span>
-        <span className="text-black text-base font-light font-noto-sans-kr">
-          개발자에게 커피 사주기
-        </span>
-        <Coffee size={15} strokeWidth={1} className="text-black" />
-      </div>
-      <div className="flex items-center space-x-1">
-        <span className='text-black text-xs font-light font-montserrat'>Copyright © SSACLE</span>
+    <footer className="flex flex-col items-center justify-center w-full h-12 px-6 py-10 mt-10 bg-ssacle-gray-sm">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="flex items-center space-x-1">
+          <span className="text-base font-medium text-black font-montserrat">
+            SSACLE
+          </span>
+          <span className="text-base font-light text-black font-noto-sans-kr">
+            개발자에게 커피 사주기
+          </span>
+          <Coffee size={15} strokeWidth={1} className="text-black" />
+        </div>
+        <div className="flex items-center space-x-1">
+          <span className="text-xs font-light text-black font-montserrat">
+            Copyright © SSACLE
+          </span>
+        </div>
       </div>
     </footer>
   )
--- frontend/src/components/layout/BaseLayout.jsx
+++ frontend/src/components/layout/BaseLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 const BaseLayout = () => {
   return (
-    <div className="flex flex-col w-full h-screen my-20">
+    <div className="flex flex-col w-full h-screen">
       <Header />
       <main className="flex-1 px-48">
         <Outlet />
