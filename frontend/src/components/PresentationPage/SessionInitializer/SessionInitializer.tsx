7312cffd15bca99e125f927e1acfabfda62c18ff
박창조 <ckdwhd1202@gmail.com>
2025-02-20 09:50:34 +0900

    feat: 서버버전으로 변경

--- frontend/src/components/PresentationPage/SessionInitializer/SessionInitializer.tsx
+++ frontend/src/components/PresentationPage/SessionInitializer/SessionInitializer.tsx
@@ -28,10 +28,10 @@ const SessionInitializer = () => {
     queryKey: ['openvidu-token'],
     queryFn: async () => {
       const sessionId = await fetchSessionId(roomId ?? 'test-session-id')
-      const token = await fetchToken(sessionId)
-      return token
-      // const serverToken = await fetchServerToken(roomId ?? 'test-session-id')
-      // return serverToken // 실제 데이터만 반환
+      // const token = await fetchToken(sessionId)
+      // return token
+      const serverToken = await fetchServerToken(roomId ?? 'test-session-id')
+      return serverToken // 실제 데이터만 반환
     },
     staleTime: 1000 * 60 * 5, // 5분 동안 캐시 유지
     gcTime: 1000 * 60 * 60, // 1시간 동안 캐시 유지
